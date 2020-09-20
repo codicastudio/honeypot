6745fc3dc70fa07c6793469879c7ace30dd86b29
codica-studio <CodicaStudioDev@gmail.com>
2020-09-20 01:18:44 -0500

    Initial

---
+++ tests/TestCase.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Spatie\Honeypot\Tests;
+
+use Illuminate\Foundation\Testing\Concerns\InteractsWithContainer;
+use Illuminate\Support\Facades\View;
+use Spatie\Honeypot\HoneypotServiceProvider;
+use Spatie\Honeypot\Tests\TestClasses\FakeEncrypter;
+
+abstract class TestCase extends \Orchestra\Testbench\TestCase
+{
+    use InteractsWithContainer;
+
+    protected $testNow = true;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        View::addLocation(__DIR__.'/views');
+
+        $this->swap('encrypter', new FakeEncrypter());
+    }
+
+    protected function getPackageProviders($app)
+    {
+        return [HoneypotServiceProvider::class];
+    }
+}
